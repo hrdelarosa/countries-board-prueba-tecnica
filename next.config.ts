af5aa4d2a0273b66b76282049c5c843367a33516
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2025-07-29 00:03:41 -0600

    fix(tsconfig): include hero.js in TypeScript compilation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flagsapi.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mainfacts.com',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
